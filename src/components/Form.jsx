e8560da42a7669e51d466442f8e19acc6820b6ba
Benjamin Xiang <benx507@gmail.com>
2021-01-17 00:25:49 -0500

    Changed Form.jsx: simplified form, and added onclick handlers to connect to backend, set state and submit when click button. Works locally, next move is to deploy

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,74 +1,84 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import React, {Component} from 'react';
 
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-        },
-    },
-}
-));
-
-export default function OutlinedButtons() {
-    const classes = useStyles();
-    return (
+export default class OutlinedButtons extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            apiResponse: "",
+            email:''};
+            
+    }
+    //changing state as you type into form
+    updateEmail = (event)=> {
+        let email = event.target.value
+        this.setState({email: email})
+        console.log("updated")
+    }
+    //submitting email to database
+    addEmail = (event)=> {
+        event.preventDefault()
+        console.log(this.state)
+        fetch('http://localhost:8000/api/email/', {
+            method: 'post',
+            body: new URLSearchParams({
+                'email': this.state.email
+            })
+        })
+    }
+    //test to see if front end server is connected to backend API
+    // callAPI() {
+    //     fetch("http://localhost:8000/")
+    //     .then(res => res.text())
+    //     .then(res => this.setState({ apiResponse: res }));
+    // }
+    
+    componentWillMount() {
+        //this.callAPI();
+    }
+    render(){
+        return (
+        <div style={{
+            textAlign:'left',
+            paddingLeft: '40px',
+            marginBottom:'40px',
+            color: "#FFFFFC",
+        }}>
+            <h1 style={{fontSize:"70px"}} ><a href="https://www.polici.org/"> polici </a></h1>
             <div style={{
-                textAlign:'left',
-                paddingLeft: '40px',
-                marginBottom:'40px',
+                textAlign: 'left',
+                paddingTop: '50px',
+                paddingBottom: '50px',
                 color: "#FFFFFC",
+                width: '50%',
             }}>
-                <h1 style={{fontSize:"70px"}} ><a href="https://www.polici.org/"> polici </a></h1>
-                <div style={{
-                    textAlign: 'left',
-                    paddingTop: '50px',
-                    paddingBottom: '50px',
-                    color: "#FFFFFC",
-                    width: '50%',
-                }}>
-                    <h2>
-                        <bf>Bringing Data to Your Day In Under Three Minutes 📈</bf>
-                    </h2>
+                <h2>
+                    <bf>Bringing Data to Your Day In Under Three Minutes 📈</bf>
+                </h2>
 
 
-                    <hr></hr>
-                    <p>
-                        Get the free, weekly email that brings data science,
-                        cutting edge research findings, and a splash of humor to the headlines.
-                        <br />
-                        <br /> <a style={{
-                        textDecoration:'underline'
-                    }}>Check out our last issue. </a>
-                    </p>
-                </div>
-                <form className={classes.root} noValidate autoComplete="on">
-                    <TextField
-                        label="Outlined"
-                        variant="filled"
-                        label="Required"
-                        defaultValue="Your Email"
-                        inputProps={{ style: { color: '#727478', backgroundColor:"#FFFFFC"}}}
-                        style={{
-                            width: "42%",
-                            color: 'black',
-                            display: 'flex',
-                            input:{
-                                color:'red'
-                            }
-                        }}
-                    />
-                </form>
-                <Button variant="contained"
-                        style={{
-                            borderRadius: 35,
-                            backgroundColor: "#FFFFFC",
-                            color:"#000000",
-                        }}>Submit</Button>
-        </div>
-    );
+                <hr></hr>
+                <p>
+                    Get the free, weekly email that brings data science,
+                    cutting edge research findings, and a splash of humor to the headlines.
+                    <br />
+                    <br /> <a style={{
+                    textDecoration:'underline'
+                }}>Check out our last issue. </a>
+                </p>
+            </div>
+            <form  noValidate autoComplete="on" onSubmit={this.addEmail}>
+                <p>Submit your email if you want to follow our Newsletter!</p>
+                <input
+        type='text'
+        onChange={this.updateEmail}
+      />
+      <input
+        type='submit'
+      />
+            </form>
+
+        <p >{this.state.apiResponse}</p>
+    </div>
+);}
+    
 }
