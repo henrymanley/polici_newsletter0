8822d64f9c0111fbda3810417d15062b9e927403
Sahar Sami <sahar.sami115@gmail.com>
2021-01-23 17:06:20 -0500

    frontend receiving validation error

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AOS from 'aos'
 import '../App.css';
 import 'aos/dist/aos.css';
-import {BrowserRouter as Router, Link, withRouter} from "react-router-dom";
+import { BrowserRouter as Router, Link, withRouter } from "react-router-dom";
 AOS.init();
 
 class OutlinedButtons extends Component {
@@ -13,21 +13,23 @@ class OutlinedButtons extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             apiResponse: "",
-            email:''};
-        AOS.init({duration: 1000});
-            
+            email: ''
+        };
+        AOS.init({ duration: 1000 });
+
     }
 
     //changing state as you type into form
-    updateEmail = (event)=> {
+    updateEmail = (event) => {
         let email = event.target.value
-        this.setState({email: email})
+        this.setState({ email: email })
         console.log("updated")
     }
+
     //submitting email to database
-    addEmail = (event)=> {
+    addEmail = (event) => {
         event.preventDefault()
         console.log(this.state)
         fetch('http://localhost:8000/api/email/', {
@@ -35,6 +37,11 @@ class OutlinedButtons extends Component {
             body: new URLSearchParams({
                 'email': this.state.email
             })
+        }).then(function (response) {
+            if (!response.ok) {
+                console.log(response.json())
+            }
+
         })
     }
 
@@ -45,75 +52,75 @@ class OutlinedButtons extends Component {
     //     .then(res => res.text())
     //     .then(res => this.setState({ apiResponse: res }));
     // }
-    
+
     componentWillMount() {
         //this.callAPI();
     }
-    render(){
+    render() {
         return (
+            <div style={{
+                textAlign: 'left',
+                paddingLeft: '40px',
+                marginBottom: '40px',
+                color: "#FFFFFC",
+                backgroundColor: "#000000"
+            }}
+            >
+                <h1 style={{ fontSize: "70px" }} data-aos="zoom-in"><a href="https://www.polici.org/"> polici </a></h1>
                 <div style={{
-                    textAlign:'left',
-                    paddingLeft: '40px',
-                    marginBottom:'40px',
+                    textAlign: 'left',
+                    paddingTop: '50px',
+                    paddingBottom: '50px',
                     color: "#FFFFFC",
-                    backgroundColor:"#000000"
-                }}
-                >
-                    <h1 style={{fontSize:"70px"}} data-aos="zoom-in"><a href="https://www.polici.org/"> polici </a></h1>
-                    <div style={{
-                        textAlign: 'left',
-                        paddingTop: '50px',
-                        paddingBottom: '50px',
-                        color: "#FFFFFC",
-                        width: '70%',
-                    }}>
-                        <h2>
-                            <bf>Bringing Data to Your Day In Under Three Minutes 📈</bf>
-                        </h2>
+                    width: '70%',
+                }}>
+                    <h2>
+                        <bf>Bringing Data to Your Day In Under Three Minutes 📈</bf>
+                    </h2>
 
 
-                        <hr></hr>
-                        <h3>
-                            Get the free, weekly email that brings data science,
-                            cutting edge research findings, and a splash of humor to the headlines.
+                    <hr></hr>
+                    <h3>
+                        Get the free, weekly email that brings data science,
+                        cutting edge research findings, and a splash of humor to the headlines.
 
                         </h3>
-                    </div>
-
-                    <div >
-                        <form  noValidate autoComplete="on" onSubmit={this.addEmail} style={{flex:'60%'}}>
-                            <h2>Try it out! 🚀</h2>
-                            <input
-                                type='text'
-                                placeholder="Email"
-                                name="email"
-                                onChange={this.updateEmail}
-                            />
-                            <div style={{flex:'40%'}}>
-                                <input
-                                    type='submit'
-                                />
-                            </div>
-
-                        </form>
-                    </div>
-
-                    <p >{this.state.apiResponse}</p>
-
-                    <Router>
-                        <Link to="./Demo" onClick={this.handleClick}>
-                            <h4 style={{
-                                textDecoration:'underline'
-                            }}>
-                                <i>Check out our last issue.</i>
-                            </h4>
-                        </Link>
-                    </Router>
-
-
-
                 </div>
-);
+
+                <div >
+                    <form noValidate autoComplete="on" onSubmit={this.addEmail} style={{ flex: '60%' }}>
+                        <h2>Try it out! 🚀</h2>
+                        <input
+                            type='text'
+                            placeholder="Email"
+                            name="email"
+                            onChange={this.updateEmail}
+                        />
+                        <div style={{ flex: '40%' }}>
+                            <input
+                                type='submit'
+                            />
+                        </div>
+
+                    </form>
+                </div>
+
+                <p >{this.state.apiResponse}</p>
+
+                <Router>
+                    <Link to="./Demo" onClick={this.handleClick}>
+                        <h4 style={{
+                            textDecoration: 'underline'
+                        }}>
+                            <i>Check out our last issue.</i>
+                        </h4>
+                    </Link>
+                </Router>
+
+
+
+            </div>
+        );
     }
 }
 
